6a6b98f4188d0c3c7aabadc8aa539647bbfc0301
Matt Ball <yosemitebandit@gmail.com>
2011-12-09 18:27:55 -0600

    a basic structure using js templates with mustache; hotlinking images to imgur

---
+++ public/projects/projects.js
@@ -0,0 +1,157 @@
+var projects = {
+    "title": "projects"
+    , "history": [
+        {
+            year: "2008"
+            , projects: [
+                {
+                    name: "microcontroller experimentation"
+                    , blurb: "started playing with 8bit AVRs"
+                    , where: "Durham"
+                    , tags: ["small"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/1lR7z.jpg"
+                    }
+                }
+                , {
+                    name: "plywood dug"
+                    , blurb: "two eight foot canoes from two sheets of 4x8 plywood"
+                    , where: "Chapel Hill"
+                    , tags: ["small"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/OdgwF.jpg"
+                    }
+                }
+                , {
+                    name: "community garden"
+                    , blurb: "building beds and RWH tanks and sheds and compost bins"
+                    , where: "Durham"
+                    , tags: ["favorite", "large"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/0g4CW.jpg"
+                    }
+                }
+                , {
+                    name: "scale"
+                    , blurb: "a fairly accurate scale from a bicycle axle"
+                    , where: "Nkokonjeru"
+                    , tags: ["small"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/EudwH.jpg"
+                    }
+                }
+                , {
+                    name: "jatropha biofuel"
+                    , blurb: "making biofuel and peanut butter in some hand-powered, small-scale expellers"
+                    , where: "Nkokonjeru"
+                    , tags: ["small"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/ao9FL.jpg"
+                    }
+                }
+            ]
+        }
+        , {
+            year: "2007"
+            , projects: [
+                {
+                    name: "soldering station"
+                    , blurb: "just a little soldering iron holder and solder dispenser"
+                    , where: "Chapel Hill"
+                    , eject: "http://www.instructables.com/id/Soldering-Station/"
+                    , tags: ["small"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/dGggf.jpg"
+                    }
+                }
+                , {
+                    name: "rainwater harvesting"
+                    , blurb: "designed and built a 2900 gallon RWH tank in rural Uganda"
+                    , where: "Kasaka/Kanoni"
+                    , eject: "http://picasaweb.google.com/mattball43/Uganda2007"
+                    , tags: ["large", "favorite"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/H2hdd.jpg"
+                    }
+                }
+                , {
+                    name: "biomass charcoal"
+                    , blurb: "researched biomass charcoal production for use in rural Uganda"
+                    , where: "Durham"
+                    , eject: "https://wiki.duke.edu/display/engineerswithoutborders/Biomass+Charcoal"
+                    , tags: ["large"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/KEu57.jpg"
+                    }
+                }
+                , {
+                    name: "archery"
+                    , blurb: "even the most primitive bows and arrows are pretty fun"
+                    , where: "Chapel Hill"
+                    , eject: "https://wiki.duke.edu/display/engineerswithoutborders/Biomass+Charcoal"
+                    , tags: ["small"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/Xe9WX.jpg"
+                    }
+                }
+            ]
+        }
+        , {
+            year: "2006"
+            , projects: [
+                {
+                    name: "trebuchet"
+                    , blurb: "building a twenty foot siege weapon"
+                    , where: "Durham"
+                    , expound: "made with my friends Ben and Jen at NCSSM"
+                    , tags: ["favorite", "large"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/I2acq.jpg"
+                        , gallery: [
+                            "../static/img/canoe_1.png"
+                            , "../static/img/canoe_2.png"
+                        ]
+                    }
+                }
+                , {
+                    name: "weatherwood cabin"
+                    , blurb: "some construction work on my family's tiny place in Virginia"
+                    , where: "Bedford"
+                    , expound: "my family and I tore down and then built back the walls of this 14x20 cabin.  We had a pro do the roof cause that's a bit hairy.  Right on the blueridge parkway and the AT."
+                    , tags: ["favorite", "large"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/qy7KP.jpg"
+                        , gallery: [
+                            "../static/img/canoe_1.png"
+                            , "../static/img/canoe_2.png"
+                        ]
+                    }
+                }
+                , {
+                    name: "ECG testers"
+                    , blurb: "manufacturing over a hundred ECG calibration devices for the developing world"
+                    , where: "Durham"
+                    , expound: "we built a whole mess of these things"
+                    , tags: ["large"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/yN0ba.jpg"
+                        , gallery: [
+                            "../static/img/canoe_1.png"
+                            , "../static/img/canoe_2.png"
+                        ]
+                    }
+                }
+                , {
+                    name: "Fire extinguisher lamp"
+                    , blurb: "making a lamp from a fire extinguisher"
+                    , where: "Durham"
+                    , eject: "http://www.instructables.com/id/Fire-Lamp!/"
+                    , tags: ["small"]
+                    , photos: {
+                        thumb: "http://i.imgur.com/AfZ0B.jpg"
+                    }
+                }
+            ]
+        }
+    ]
+};
