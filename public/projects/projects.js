65945690ecfd03cc9afba9a06c53717b38ab1515
Matt Ball <yosemitebandit@gmail.com>
2011-12-11 17:04:07 -0600

    adds the scale project

--- public/projects/projects.js
+++ public/projects/projects.js
@@ -219,10 +219,24 @@ var projects = {
                     }
                 }
                 , {
-                    name: "scale"
+                    name: "scrap scale"
                     , blurb: "a fairly accurate scale from a bicycle axle"
                     , where: "Nkokonjeru"
-                    , tags: ["small"]
+                    , tags: ['small', 'favorite']
+                    , expound: "<p>When the electricity went off in Nkokonjeru,"
+                        + " I (finally) had an excuse to stop welding the gates for RASD, the NGO where we worked."
+                        + "  Some other projects at the site needed to weigh things so I tried making a scale.</p>"
+                        + "<div class='embedded-content'><img src='http://i.imgur.com/Vjm9F.jpg' /></div>"
+                        + "<p>I used a moment-balance system where a known volume of water (usually 500mL)"
+                        + " would be shifted until it balanced the load on the other side of the scale."
+                        + "  It's somewhat similar to a physician's scale for weighing people."
+                        + "  I used a bike hub and some tough wood to form the scale's body;"
+                        + " scrap metal on the short end helped cancel the weight of the long arm.</p>"
+                        + "<p>When it was working, Abhinav Kapur and I estimated a 40g precision."
+                        + "  I made the mistake of taking the balance apart before finishing a second, nicer version."
+                        + "  Version two, of course, was never completed."
+                        + "  Sigh. I'd like to put one together in the States and employ the convenience of so many nice power tools,"
+                        + " it would be handy to have a scale around.</p>"
                     , photos: {
                         thumb: "http://i.imgur.com/EudwH.jpg"
                     }
