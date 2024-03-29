e81142ff63c05bc074eaf5ada8b164ab8187a1f1
Matt Ball <matt.ball.2@gmail.com>
2024-03-29 16:34:10 -0700

    more params

--- om2/drawings/draw.py
+++ om2/drawings/draw.py
@@ -15,7 +15,7 @@ import random
 
 canvas = (400.0, 400.0)
 header = (
-    f"<svg xmlns='http://www.w3.org/2000/svg' "
+    "<svg xmlns='http://www.w3.org/2000/svg' "
     f"width='{canvas[0]}' "
     f"height='{canvas[1]}' "
     ">"
@@ -23,33 +23,33 @@ header = (
 footer = "</svg>"
 svg_elements = []
 
-branches = 32
+branches = 64
 ticks = 256
 
 for branch in range(branches):
-    twig = [
-        (
-            canvas[0] / 2,
-            canvas[1],
-            0.0,
-        ),
-    ]
+    twig = [(canvas[0] / 2, canvas[1], 0.0, 0.0)]
     for tick in range(ticks):
         twig.append(
             (
                 twig[-1][0] + 10 * random.random() * random.choice((-1, 1)),
                 twig[-1][1] - 10 * random.random(),
                 twig[-1][2] + 10 * random.random(),
+                ticks / (tick + 1) * 0.1 * random.random(),
             )
         )
 
     for index, node in enumerate(twig):
         if index == 0:
             continue
-        x1, y1, z1 = twig[index - 1]
-        x2, y2, z2 = node
+        x1, y1, z1, sw1 = twig[index - 1]
+        x2, y2, z2, sw2 = node
         svg_elements.append(
-            f"<line x1='{x1}' y1='{y1}' x2='{x2}' y2='{y2}' stroke='black' />"
+            "<line "
+            f"x1='{x1}' y1='{y1}' "
+            f"x2='{x2}' y2='{y2}' "
+            f"stroke='darkgreen' "
+            f"stroke-width='{sw2}' "
+            "/>"
         )
 
 with open("out.svg", "w") as outfile:
